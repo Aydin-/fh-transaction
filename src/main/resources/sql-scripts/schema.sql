28eb9a7497e4503b2632621ffced21b2626c9aa6
Aydin Gungordu <aydinbg@gmail.com>
2019-03-18 16:19:35 +0000

    Add customer info table and data in H2

--- src/main/resources/sql-scripts/schema.sql
+++ src/main/resources/sql-scripts/schema.sql
@@ -14,3 +14,45 @@ CREATE TABLE USER (
   username varchar(255) NOT NULL,
   PRIMARY KEY (id)
 );
+
+CREATE TABLE CUSTOMER(
+   customerInfoid             BIT  NOT NULL PRIMARY KEY
+  ,customerInfocreated_at     VARCHAR(19) NOT NULL
+  ,customerInfoupdated_at     VARCHAR(19) NOT NULL
+  ,customerInfodeleted_at     VARCHAR(4)
+  ,customerInfonumber         VARCHAR(16) NOT NULL
+  ,customerInfoexpiryMonth    INTEGER  NOT NULL
+  ,customerInfoexpiryYear     INTEGER  NOT NULL
+  ,customerInfostartMonth     VARCHAR(4)
+  ,customerInfostartYear      VARCHAR(30)
+  ,customerInfoissueNumber    VARCHAR(30)
+  ,customerInfoemail          VARCHAR(17) NOT NULL
+  ,customerInfobirthday       VARCHAR(19) NOT NULL
+  ,customerInfogender         VARCHAR(30)
+  ,customerInfobillingTitle   VARCHAR(30)
+  ,customerInfoAuthorization  VARCHAR(204) NOT NULL
+  ,billingFirstName           VARCHAR(7) NOT NULL
+  ,billingLastName            VARCHAR(4) NOT NULL
+  ,billingCompany             VARCHAR(30)
+  ,billingAddress1            VARCHAR(12) NOT NULL
+  ,billingAddress2            VARCHAR(30)
+  ,billingCity                VARCHAR(7) NOT NULL
+  ,billingPostcode            INTEGER  NOT NULL
+  ,billingState               VARCHAR(30)
+  ,billingCountry             VARCHAR(3) NOT NULL
+  ,billingPhone               VARCHAR(30)
+  ,billingFax                 VARCHAR(30)
+  ,shippingTitle              VARCHAR(30)
+  ,shippingFirstName          VARCHAR(7) NOT NULL
+  ,shippingLastName           VARCHAR(4) NOT NULL
+  ,shippingCompany            VARCHAR(30)
+  ,shippingAddress1           VARCHAR(12) NOT NULL
+  ,shippingAddress2           VARCHAR(30)
+  ,shippingCity               VARCHAR(7) NOT NULL
+  ,shippingPostcode           INTEGER  NOT NULL
+  ,shippingState              VARCHAR(30)
+  ,shippingCountry            VARCHAR(2) NOT NULL
+  ,shippingPhone              VARCHAR(30)
+  ,shippingFax                VARCHAR(30)
+);
+
