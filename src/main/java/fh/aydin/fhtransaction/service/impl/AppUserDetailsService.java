95bae969566ceafc752660ede807bdeb486bd2a7
Aydin Gungordu <aydinbg@gmail.com>
2019-03-18 16:29:03 +0000

    Remove unused import

--- src/main/java/fh/aydin/fhtransaction/service/impl/AppUserDetailsService.java
+++ src/main/java/fh/aydin/fhtransaction/service/impl/AppUserDetailsService.java
@@ -1,6 +1,5 @@
 package fh.aydin.fhtransaction.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
