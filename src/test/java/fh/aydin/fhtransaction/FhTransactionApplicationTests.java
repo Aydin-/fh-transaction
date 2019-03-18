4bee04f8851f63d46880dae0a49a19a989596969
Aydin Gungordu <aydinbg@gmail.com>
2019-03-18 15:14:29 +0000

    Add tests

--- src/test/java/fh/aydin/fhtransaction/FhTransactionApplicationTests.java
+++ src/test/java/fh/aydin/fhtransaction/FhTransactionApplicationTests.java
@@ -1,16 +1,99 @@
 package fh.aydin.fhtransaction;
 
+import static org.hamcrest.Matchers.is;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
+
+import fh.aydin.fhtransaction.FhTransactionApplication;
+
+import org.springframework.boot.json.JacksonJsonParser;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebAppConfiguration
+@SpringBootTest(classes = FhTransactionApplication.class)
+@ActiveProfiles("mvc")
 public class FhTransactionApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private WebApplicationContext wac;
+
+    @Autowired
+    private FilterChainProxy springSecurityFilterChain;
+
+    private MockMvc mockMvc;
+
+    private static final String CLIENT_ID = "testjwtclientid";
+    private static final String CLIENT_SECRET = "XY7kmzoNzl100";
+
+    private static final String CONTENT_TYPE = "application/json;charset=UTF-8";
+
+    private static final Integer MERCHANT_ID = 1;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).addFilter(springSecurityFilterChain).build();
+    }
+
+    private String obtainAccessToken(String username, String password) throws Exception {
+        final MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "password");
+        params.add("client_id", CLIENT_ID);
+        params.add("username", username);
+        params.add("password", password);
+
+        // @formatter:off
+
+        ResultActions result = mockMvc.perform(post("/oauth/token")
+                               .params(params)
+                               .with(httpBasic(CLIENT_ID, CLIENT_SECRET))
+                               .accept(CONTENT_TYPE))
+                               .andExpect(status().isOk())
+                               .andExpect(content().contentType(CONTENT_TYPE));
+        
+        // @formatter:on
+
+        String resultString = result.andReturn().getResponse().getContentAsString();
+
+        JacksonJsonParser jsonParser = new JacksonJsonParser();
+        return jsonParser.parseMap(resultString).get("access_token").toString();
+    }
+
+    @Test
+    public void givenNoToken_whenGetSecureRequest_thenUnauthorized() throws Exception {
+        mockMvc.perform(get("/api/transactions")).andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void givenToken_whenPostGetSecureRequest_thenOk() throws Exception {
+        final String accessToken = obtainAccessToken("admin", "jwtpass");
+        
+        mockMvc.perform(get("/api/transactions")
+                .header("Authorization", "Bearer " + accessToken)
+                .accept(CONTENT_TYPE))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(CONTENT_TYPE))
+                .andExpect(jsonPath("$.[0].merchantId", is(MERCHANT_ID)));
+        
+        // @formatter:on
+    }
 
 }
